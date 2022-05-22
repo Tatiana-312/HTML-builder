779a911dc4fe3846307d858b70e4baa65938cbff
Tatiana-312 <tatyanapetrosh@gmail.com>
2022-05-22 18:38:09 +0300

    feat: complete fourth task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,9 +8,9 @@ fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
       fs.stat(path.join(__dirname, 'secret-folder', file), (err, stats) => {
         if (err) throw err;
         if (stats.isFile()) {
-          let fileSizeInBytes = stats.size / 1024;
+          let fileSizeInBytes = stats.size;
           let fileExtension = (path.extname(file)).slice(1);
-          console.log(`${file} - ${fileExtension} - ${fileSizeInBytes}kb`);
+          console.log(`${file.slice(0, file.length - (fileExtension.length + 1))} - ${fileExtension} - ${fileSizeInBytes}bytes`);
         }
       });
     });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir(srcOrigDir, srcCopyDir) {
+  await fs.rm(srcCopyDir, { recursive: true, force: true });
+  const entries = await fs.readdir(srcOrigDir, { withFileTypes: true });
+  await fs.mkdir(srcCopyDir, { recursive: true });
+  for(let entry of entries) {
+    const srcOrigDirPath = path.join(srcOrigDir, entry.name);
+    const srcCopyDirPath = path.join(srcCopyDir, entry.name);
+    if(entry.isDirectory()) {
+      await copyDir(srcOrigDirPath, srcCopyDirPath);
+    } else {
+      await fs.copyFile(srcOrigDirPath, srcCopyDirPath);
+    }
+  }
+}
+
+copyDir(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
