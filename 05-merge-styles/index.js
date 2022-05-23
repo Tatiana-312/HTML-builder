0ae1f0bc3c116b8edf71be7f2ca171f37f3143b0
Tatiana-312 <tatyanapetrosh@gmail.com>
2022-05-23 13:02:34 +0300

    feat: complete fifth task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
+  if (err) {
+    throw err;
+  } else {
+    files.forEach(file => {
+      fs.stat(path.join(__dirname, 'styles', file), (err, stats) => {
+        if (err) throw err;
+        if (stats.isFile() && path.extname(file) === '.css') {
+          const input = fs.createReadStream(path.join(__dirname, 'styles', file), { encoding: 'utf-8', flags: 'r' });
+          const output = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'), { encoding: 'utf-8', flags: 'a' });
+          input.pipe(output);
+        }
+      });
+    });
+  }
+});
+
+
+
